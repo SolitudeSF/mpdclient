5c1cb6bba66a2c74bd02b2ae77a007e6b3383dec
Zestyr <zestyr@protonmail.com>
2019-05-04 18:58:57 +0200

    Update example to more closely reflect original

--- examples/example.nim
+++ examples/example.nim
@@ -1,44 +1,44 @@
+# based on https://raw.githubusercontent.com/cmende/libmpdclient/master/src/example.c
+
 import mpdclient
-from times import inMilliseconds
+import times
 
-let
-  mpd = newMPDClient()
-  status = mpd.status
+let mpd = newMPDClient()
 
+let status = mpd.status
 echo "volume: ", status.volume
 echo "repeat: ", status.repeat
-echo "queueVersion: ", status.queueVersion
-echo "queueLen: ", status.queueLen
+echo "queue version: ", status.queueVersion
+echo "queue length: ", status.queueLen
 
 if status.error.isSome:
   echo "error: ", status.error.get
 
 if status.state in {statePlay, statePause}:
-  echo "song: ", status.song
-  echo "elaspedTime: ",status.elapsed
+  if status.song.isSome:
+    echo "song: ", status.song.get.pos
+  echo "elaspedTime: ", status.elapsed.inSeconds
   echo "elasped_ms: ", status.elapsed.inMilliseconds
-  echo "totalTime: ", status.duration
-  echo "bitRate: ", status.audio.bits
+  echo "totalTime: ", status.duration.inSeconds
+  echo "bitRate: ", status.bitrate
 
 let audioFormat = status.audio
-
 echo "sampleRate: ", audioFormat.rate
 echo "bits: ", audioFormat.bits
 echo "channels: ", audioFormat.channels
 
-
 let song = mpd.currentSong
-
 if song.isSome:
   let song = song.get
-  echo "file: ", song.file
+  echo "uri: ", song.file
   echo "artist: ", song.artist
-  echo "title: ", song.title
-  echo "name: ", song.name
   echo "album: ", song.tags[$tagAlbum]
+  echo "title: ", song.title
+  echo "track: ", song.tags[$tagTrack]
+  echo "name: ", song.name
   echo "date: ", song.tags[$tagDate]
 
-  echo "time: ", song.duration
+  echo "time: ", song.duration.inSeconds
   echo "pos: ", song.place.get.pos
 
 for playlist in mpd.listPlaylists:
